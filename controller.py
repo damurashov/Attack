2b1c49077ea9e472821210f20ac9e87e949486b4
DmitryMurashov <dmtr.murashov@gmail.com>
2021-06-12 21:54:00 +0300

    [controller] dt | Removed manual dt calculation, since the library does it for us

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@ import pioneer_sdk
 import time
 import math
 from threedvector import Vector
+import os
 
 
 class RcWrapper(pioneer_sdk.Pioneer):
@@ -59,11 +60,9 @@ class AttackStrategy(RcWrapper):
 
 		self.last_time_seconds = None
 		self.last_offset_horizontal = None
-		self.last_offset_vertical = None
 		self.target_lost = None
 
 	def reset_pid(self):
-		self.last_time_seconds = None
 		self.last_offset_horizontal = None
 		self.last_offset_vertical = None
 		self.target_lost = None
@@ -97,18 +96,11 @@ class AttackStrategy(RcWrapper):
 		self.last_offset_vertical = offset_vertical
 		self.target_lost = False
 
-		delta_time_seconds = self.last_time_seconds
-		if self.last_time_seconds is None:
-			self.last_time_seconds = time.time()
-
-		if delta_time_seconds is not None:
-			delta_time_seconds = self.last_time_seconds - delta_time_seconds
-
 		if self.should_engage():
 			self.engage()
 
-		self.set_rc('throttle', self.get_normalized_output_vertical(self.pid_vertical(offset_vertical, delta_time_seconds)))
-		self.set_rc('yaw', self.get_normalized_output_horizontal(self.pid_horizontal(offset_horizontal, delta_time_seconds)))
+		self.set_rc('throttle', self.get_normalized_output_vertical(self.pid_vertical(offset_vertical)))
+		self.set_rc('yaw', self.get_normalized_output_horizontal(self.pid_horizontal(offset_horizontal)))
 
 	def on_target_lost(self):
 		self.target_lost = True
