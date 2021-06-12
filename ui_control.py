444b82d345f9b7e955dfc7e5721f8e8fa7f460fa
DmitryMurashov <dmtr.murashov@gmail.com>
2021-06-12 13:56:33 +0300

    [ui_control] Tuned manual control

--- ui_control.py
+++ ui_control.py
@@ -9,7 +9,7 @@ import keyboard
 import cv2
 
 
-class ParametersAnglesPid:
+class ParametersPidAngles:
 	P_VERTICAL_PID = 1
 	I_VERTICAL_PID = 0.1
 	D_VERTICAL_PID = 0.05
@@ -19,7 +19,7 @@ class ParametersAnglesPid:
 	D_HORIZONTAL_PID = 0.05
 
 
-class ParametersPixelsPid:
+class ParametersPidPixels:
 	P_VERTICAL_PID = 1
 	I_VERTICAL_PID = 0.1
 	D_VERTICAL_PID = 0.05
@@ -46,61 +46,36 @@ def getarparser():
 
 class UiControl:
 	def __init__(self):
-		self.thread_rc_pid = None
-		self.controller = None
-		self.camera = None
-		self.pid_type = None
+		self.controller = UiControl.__instantiate_controller()
+		self.thread_rc_pid = UiControl.__instantiate_thread_rc(self.controller)
+		self.tracker = None
+		self.__instantiate_key_mappings()
 
-		self.__instantiate_strategy()  # Control
-		self.__instantiate_thread_rc()
+	@staticmethod
+	def __instantiate_thread_rc(controller: AttackStrategy):
+		thread_rc_pid = threading.Thread(target=controller.push_rc_task)
+		thread_rc_pid.start()
+		return thread_rc_pid
 
-	def __instantiate_thread_rc(self, rc_pid: AttackStrategy):
-		self.thread_rc_pid = threading.Thread(target=self.controller.push_rc_task)
-		self.thread_rc_pid.start()
+	@staticmethod
+	def __instantiate_controller():
+		pid_input = getarparser().parse_args().pid_input
 
-	def __instantiate_strategy(self):
-		opts = getarparser().parse_args()
-		self.pid_type = opts.pid_input
+		pid_to_class_mapping = {
+			'pixels': (ParametersPidPixels, AttackStrategyPixels),
+			'angles': (ParametersPidAngles, AttackStrategyAngles),
+		}
 
-		StrategyClass = object
-		if self.pid_type == 'pixels':
-			StrategyClass = ParametersPixelsPid
-		elif self.pid_type == 'angles':
-			StrategyClass = ParametersAnglesPid
+		ParametersClass, StrategyClass = pid_to_class_mapping[pid_input]
 
-		pid_vertical = PID(StrategyClass.P_VERTICAL_PID, StrategyClass.I_VERTICAL_PID, StrategyClass.D_VERTICAL_PID, SETPOINT, SAMPLE_TIME)
-		pid_horizontal = PID(StrategyClass.P_HORIZONTAL_PID, StrategyClass.I_HORIZONTAL_PID, StrategyClass.D_HORIZONTAL_PID, SETPOINT, SAMPLE_TIME)
+		pid_vertical = PID(ParametersClass.P_VERTICAL_PID, ParametersClass.I_VERTICAL_PID, ParametersClass.D_VERTICAL_PID, SETPOINT, SAMPLE_TIME)
+		pid_horizontal = PID(ParametersClass.P_HORIZONTAL_PID, ParametersClass.I_HORIZONTAL_PID, ParametersClass.D_HORIZONTAL_PID, SETPOINT, SAMPLE_TIME)
 
-		self.controller = StrategyClass(pid_vertical, pid_horizontal)
+		return StrategyClass(pid_vertical, pid_horizontal)
 
-	def __instantiate_camera(self):  # Initiates engage mode
-		opts = getarparser().parse_args()
-		self.camera = Camera(opts)
-
-	def __map_rc_channel_toggle(self, kb_key, rc_channel, value, reset_on_release=True):
-		keyboard.add_hotkey(kb_key, self.controller.set_rc, args=(rc_channel, value,))
-		if reset_on_release:
-			keyboard.on_release_key(kb_key, self.controller.reset_rc)
-
-	def engage_mode(self):
-		self.__instantiate_camera()
-
-		while True:
-			img = self.camera.get_frame()
-
-			if img is None:
-				continue
-
-			bbox, state = self.camera.track(img)
-			Camera.visualize_tracking(img, bbox, state)
-
-			hv_positions = Camera.center_positions(bbox, img, type=self.pid_type)
-			self.controller.on_target(*tuple(hv_positions))
-
-			key = cv2.waitKey(1)
-
-			if key == 27:  # esc
-				break
+	@staticmethod
+	def __instantiate_tracker():  # Initiates engage mode
+		return Camera(getarparser().parse_args())
 
 	def __instantiate_key_mappings(self):
 		self.__map_rc_channel_toggle('w', 'pitch', 1.0)
@@ -111,11 +86,42 @@ class UiControl:
 		self.__map_rc_channel_toggle('e', 'yaw', 1.0)
 		self.__map_rc_channel_toggle('shift+w', 'throttle', 1.0)
 		self.__map_rc_channel_toggle('shift+s', 'throttle', -1.0)
-		self.__map_rc_channel_toggle('0', 'mode', 0, False)
-		self.__map_rc_channel_toggle('1', 'mode', 1, False)
-		self.__map_rc_channel_toggle('2', 'mode', 2, False)
+		keyboard.add_hotkey("0", self.controller.set_rc, args=('mode', 0))
+		keyboard.add_hotkey("1", self.controller.set_rc, args=('mode', 1))
+		keyboard.add_hotkey("2", self.controller.set_rc, args=('mode', 2))
 		keyboard.add_hotkey('ctrl+a', self.controller.arm)
 		keyboard.add_hotkey('ctrl+d', self.controller.disarm)
 		# keyboard.add_hotkey('ctrl+e', self.engage_mode)
 
+	def __map_rc_channel_toggle(self, kb_key, rc_channel, value, reset_on_release=True):
+		keyboard.add_hotkey(kb_key, self.controller.set_rc, args=(rc_channel, value,))
+		# keyboard.on_press_key(kb_key, lambda: self.controller.set_rc(rc_channel, value))
+		if reset_on_release:
+			# keyboard.add_hotkey(kb_key, self.controller.set_rc, args=(rc_channel, 0.0,), trigger_on_release=True)
+			keyboard.on_release_key(kb_key.split('+')[-1], lambda e: self.controller.set_rc(rc_channel, 0.0))
 
+	def engage_mode(self):
+		self.tracker = UiControl.__instantiate_tracker()
+
+		while True:
+			img = self.tracker.get_frame()
+
+			if img is None:
+				continue
+
+			bbox, state = self.tracker.track(img)
+			Camera.visualize_tracking(img, bbox, state)
+
+			hv_positions = Camera.center_positions(bbox, img, type=self.pid_input)
+			self.controller.on_target(*tuple(hv_positions))
+
+			key = cv2.waitKey(1)
+
+			if key == 27:  # esc
+				break
+
+
+if __name__ == "__main__":
+	ui_control = UiControl()
+	while True:
+		pass
